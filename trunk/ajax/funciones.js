377ba0b6767f959b3516f2d1d05dce11734ce43c
dadagama <dadagama@e7c1c25a-dc2c-11de-b4e8-61bb9a648e99>
2009-12-06 06:43:02 +0000

    se agrega validacion al formulario de login

--- trunk/ajax/funciones.js
+++ trunk/ajax/funciones.js
@@ -11,20 +11,23 @@ function inicializarEventos()
 
 function verificar_datos_sesion()
 {
-	var usu_login = $("#usu_login").val();
-	var usu_password = $("#usu_password").val();
-	$.ajax({
-				async:		true,
-				type: 		"POST",
-				dataType:	"html",
-				contentType:"application/x-www-form-urlencoded",
-				url:		"servidor.php",
-				data:		"usu_login="+usu_login+"&usu_password="+usu_password,
-				beforeSend:	ajax_send,
-				success:	iniciar_sesion,
-				timeout:	4000,
-				error:		ajax_error("Hubo un error al establecer conexión con el servidor.")
-			}); 
+	if (validarCampoNoVacio($("#usu_login"), "Usuario", true) && validarCampoNoVacio($("#usu_password"), "Contraseña", true))
+	{
+		var usu_login = $("#usu_login").val();
+		var usu_password = $("#usu_password").val();
+		$.ajax({
+					async:		true,
+					type: 		"POST",
+					dataType:	"html",
+					contentType:"application/x-www-form-urlencoded",
+					url:		"../servidor/servidor.php",
+					data:		"usu_login="+usu_login+"&usu_password="+usu_password,
+					beforeSend:	ajax_send,
+					success:	iniciar_sesion,
+					timeout:	4000,
+					error:		ajax_error("Hubo un error al establecer conexión con el servidor.")
+				}); 
+	}
 	return false;
 }
 
@@ -32,13 +35,18 @@ function iniciar_sesion(div_conexiones)
 {
 	if(div_conexiones != '-1')
 	{
+		setTimeout('ocultar("div_sesion")',0);
 		$("#div_cuerpo").append(div_conexiones);
 		$('#div_conexiones').hide();
-		$("#div_sesion").fadeOut(1000);
 		setTimeout('mostrar("div_conexiones")',1000);
 	}
 	else
 		ajax_error("El usuario no existe o la contraseña es incorrecta.");
+}
+
+function ocultar(elemento)
+{
+	$("#"+elemento).fadeOut(1000);
 }
 
 function mostrar(elemento)
