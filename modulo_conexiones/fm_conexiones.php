ab8736368a5a6533cfc546e037a96120a105c9ff
dadagama <dadagama@e7c1c25a-dc2c-11de-b4e8-61bb9a648e99>
2009-12-09 04:35:06 +0000

    se añaden nuevas palabras a traducciones y nuevos estilos para los divs conexiones

--- modulo_conexiones/fm_conexiones.php
+++ modulo_conexiones/fm_conexiones.php
@@ -23,7 +23,26 @@
 			
 				//FORMULARIO
 				$html->tag("div", array("class"=>"fila"));
-					$html->tag("div", array("class"=>"tabla_centrada"));
+					$html->tag("div", array("class"=>"tabla_centrada tabla_bloque alto_120"));
+					
+						//CAMPO TIPO CONEXION
+						$html->tag("div", array("class"=>"fila"));
+							$html->tag("div", array("class"=>"celda alto_30"));
+								$html->tag("label", array("class"=>"etiqueta"));
+									$html->printText("lbl_con_tipo");
+								$html->end("label");
+							$html->end("div");
+							$html->tag("div", array("class"=>"celda alto_30"));
+								$html->tag("select", array("class"=>"ancho_120", "id"=>"con_tipo", "title"=>$html->getText('ttp_con_tipo')));
+									$html->tag("option", array("value"=>"bd"));
+										$html->printText("opt_con_tipo_bd");
+									$html->end("option");
+									$html->tag("option", array("value"=>"archivo"));
+										$html->printText("opt_con_tipo_archivo");
+									$html->end("option");
+								$html->end("select");
+							$html->end("div");
+						$html->end("div");
 					
 						//CAMPO NOMBRE CONEXION
 						$html->tag("div", array("class"=>"fila"));
@@ -33,7 +52,7 @@
 								$html->end("label");
 							$html->end("div");
 							$html->tag("div", array("class"=>"celda alto_30"));
-								$html->tag("input", array("class"=>"ancho_100", "id"=>"con_nombre", "type"=>"text", "size"=>"50", "title"=>$html->getText('ttp_con_nombre')));
+								$html->tag("input", array("class"=>"ancho_120", "id"=>"con_nombre", "type"=>"text", "maxlength"=>"20", "title"=>$html->getText('ttp_con_nombre')));
 							$html->end("div");
 						$html->end("div");
 						
@@ -45,7 +64,7 @@
 								$html->end("label");
 							$html->end("div");
 							$html->tag("div", array("class"=>"celda alto_30"));
-								$html->tag("input", array("class"=>"ancho_100", "id"=>"con_usuario", "type"=>"text", "size"=>"50", "title"=>$html->getText("ttp_con_usuario")));
+								$html->tag("input", array("class"=>"ancho_120", "id"=>"con_usuario", "type"=>"text", "maxlength"=>"30", "title"=>$html->getText("ttp_con_usuario")));
 							$html->end("div");
 						$html->end("div");
 						
@@ -57,7 +76,7 @@
 								$html->end("label");
 							$html->end("div");
 							$html->tag("div", array("class"=>"celda alto_30"));
-								$html->tag("input", array("class"=>"ancho_100", "id"=>"con_password", "type"=>"text", "size"=>"50", "title"=>$html->getText('ttp_con_password')));
+								$html->tag("input", array("class"=>"ancho_120", "id"=>"con_password", "type"=>"password", "maxlength"=>"30", "title"=>$html->getText('ttp_con_password')));
 							$html->end("div");
 						$html->end("div");
 					$html->end("div");
@@ -84,7 +103,7 @@
 	
 	
 	//DIV DE CONEXIONES
-	/*$html->tag("div", array("id"=>"div_conexiones"));
+	$html->tag("div", array("id"=>"div_conexiones"));
 		$html->tag("fieldset");
 		
 			$html->tag("legend");
@@ -93,15 +112,15 @@
 				$html->end("label");
 			$html->end("legend");
 			//TABLA
-			$html->tag("div", array("class"=>"tabla"));
+			/*$html->tag("div", array("class"=>"tabla"));
 				$html->tag("div", array("class"=>"fila"));
 						$html->tag("div", array("class"=>"tabla_centrada"));
 							$html->tag("div", array("class"=>"fila"));
 								$html->tag("div", array("class"=>"celda alto_30"));
 									$html->tag("label", array("class"=>"etiqueta"));
-										$html->printText("lbl_con_nombre");
+										$html->printText("lbl_con_nombre");*/
 										
 		$html->end("fieldset");
-	$html->end("div");*/ 
+	$html->end("div");
 	//FIN DIV DE CONEXIONES
 ?>
