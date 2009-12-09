8df603547d42bbea93130956fb1ad373be239d92
dadagama <dadagama@e7c1c25a-dc2c-11de-b4e8-61bb9a648e99>
2009-12-09 04:35:06 +0000

    se añaden nuevas palabras a traducciones y nuevos estilos para los divs conexiones

--- trunk/sql/BDI_inicial.sql
+++ trunk/sql/BDI_inicial.sql
@@ -17,6 +17,7 @@ CREATE TABLE IF NOT EXISTS `PSDG_idioma` (
 ) ENGINE= InnoDB COMMENT = 'Almacena las traducciones de las etiquetas';
 
 /*ESPAÑOL*/
+/*login*/
 INSERT INTO PSDG_idioma VALUES('lbl_login','es','Iniciar Sesión');
 INSERT INTO PSDG_idioma VALUES('lbl_usu_login','es','Usuario:');
 INSERT INTO PSDG_idioma VALUES('lbl_usu_password','es','Contraseña:');
@@ -25,15 +26,23 @@ INSERT INTO PSDG_idioma VALUES('ttp_usu_password','es','Digite su contraseña');
 INSERT INTO PSDG_idioma VALUES('ttp_chk_lang_en','es','Inglés');
 INSERT INTO PSDG_idioma VALUES('ttp_chk_lang_es','es','Español');
 INSERT INTO PSDG_idioma VALUES('btn_login','es','Iniciar Sesión');
-INSERT INTO PSDG_idioma VALUES('lbl_conexion','es','Conexión');
+/*conexion*/
+INSERT INTO PSDG_idioma VALUES('lbl_conexion','es','Configuración de conexiones');
+INSERT INTO PSDG_idioma VALUES('lbl_con_tipo','es','Conectar con:');
+INSERT INTO PSDG_idioma VALUES('ttp_con_tipo','es','Tipo de conexión');
+INSERT INTO PSDG_idioma VALUES('opt_con_tipo_archivo','es','Archivo');
+INSERT INTO PSDG_idioma VALUES('opt_con_tipo_bd','es','Base de datos');
 INSERT INTO PSDG_idioma VALUES('lbl_con_nombre','es','Nombre conexión:');
-INSERT INTO PSDG_idioma VALUES('ttp_con_nombre','es','Nombre que identifica la conexión en la aplicación');
+INSERT INTO PSDG_idioma VALUES('ttp_con_nombre','es','Nombre que se utilizará para identificar la conexión');
 INSERT INTO PSDG_idioma VALUES('lbl_con_usuario','es','Usuario:');
 INSERT INTO PSDG_idioma VALUES('ttp_con_usuario','es','Usuario con el que se conecta a la Base de datos');
 INSERT INTO PSDG_idioma VALUES('lbl_con_password','es','Contraseña:');
-INSERT INTO PSDG_idioma VALUES('ttp_con_password','es','Contraseña del usuario en la Base de datos');
-INSERT INTO PSDG_idioma VALUES('btn_establecer','es','Establecer');
+INSERT INTO PSDG_idioma VALUES('ttp_con_password','es','Contraseña del usuario para la Base de datos');
+INSERT INTO PSDG_idioma VALUES('btn_establecer','es','Añadir');
+INSERT INTO PSDG_idioma VALUES('lbl_conexiones_establecidas','es','Conexiones establecidas');
+/**************************************************************************************************************/
 /*INGLES*/
+/*login*/
 INSERT INTO PSDG_idioma VALUES('lbl_login','en','Log in');
 INSERT INTO PSDG_idioma VALUES('lbl_usu_login','en','User:');
 INSERT INTO PSDG_idioma VALUES('lbl_usu_password','en','Password:');
@@ -42,12 +51,18 @@ INSERT INTO PSDG_idioma VALUES('ttp_usu_password','en','Type your password');
 INSERT INTO PSDG_idioma VALUES('ttp_chk_lang_en','en','English');
 INSERT INTO PSDG_idioma VALUES('ttp_chk_lang_es','en','Spanish');
 INSERT INTO PSDG_idioma VALUES('btn_login','en','Sign in');
-INSERT INTO PSDG_idioma VALUES('lbl_conexion','en','Connection');
+/*conexion*/
+INSERT INTO PSDG_idioma VALUES('lbl_conexion','en','Connection setup');
+INSERT INTO PSDG_idioma VALUES('lbl_con_tipo','en','Link to:');
+INSERT INTO PSDG_idioma VALUES('ttp_con_tipo','en','Connection type');
+INSERT INTO PSDG_idioma VALUES('opt_con_tipo_archivo','en','File');
+INSERT INTO PSDG_idioma VALUES('opt_con_tipo_bd','en','Database');
 INSERT INTO PSDG_idioma VALUES('lbl_con_nombre','en','Connection name:');
 INSERT INTO PSDG_idioma VALUES('ttp_con_nombre','en','Name that identifies the connection in the application');
 INSERT INTO PSDG_idioma VALUES('lbl_con_usuario','en','User:');
 INSERT INTO PSDG_idioma VALUES('ttp_con_usuario','en','User that connects to the database');
 INSERT INTO PSDG_idioma VALUES('lbl_con_password','en','Password:');
 INSERT INTO PSDG_idioma VALUES('ttp_con_password','en','User\'s password in the database');
-INSERT INTO PSDG_idioma VALUES('btn_establecer','en','Set');
+INSERT INTO PSDG_idioma VALUES('btn_establecer','en','Add');
+INSERT INTO PSDG_idioma VALUES('lbl_conexiones_establecidas','en','Established connections');
 
