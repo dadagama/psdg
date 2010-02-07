21aec9771e3a3f6f9227b7eaddd05bb41e0e04d1
dadagama <dadagama@e7c1c25a-dc2c-11de-b4e8-61bb9a648e99>
2010-02-07 23:12:21 +0000

    Agregado archivo leeme.txt , licencia gpl v3 y encabezados de licencia

--- trunk/sql/sqlRicardo.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `BDI`.`PSDG_idioma` (`idi_texto`, `idi_lenguaje`, `idi_traduccion`) VALUES ('rec_lbl_lambda', 'en', 'Lambda'), ('rec_lbl_lambda', 'es', 'Lambda');
-INSERT INTO `BDI`.`PSDG_idioma` (`idi_texto`, `idi_lenguaje`, `idi_traduccion`) VALUES ('rec_lbl_media', 'en', 'Mean'), ('rec_lbl_media', 'es', 'Media');
-INSERT INTO `BDI`.`PSDG_idioma` (`idi_texto`, `idi_lenguaje`, `idi_traduccion`) VALUES ('rec_lbl_desviacion_estandar', 'es', 'Desviacion estandar'), ('rec_lbl_desviacion_estandar', 'en', 'Standard deviation');
